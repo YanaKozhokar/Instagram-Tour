63d219ec42c3f3e03166c12e6217e782ffe78e5f
yanakozhokar <yanakozhokar1996@gmail.com>
2023-02-03 21:25:40 +0000

    Deploying to gh-pages from @ yanakozhokar/instagram-tour@ef144460613e677fb4c8fa58d3dd10a89a2625d1 🚀

---
+++ index.3918df2b.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",loop:!0,spaceBetween:30,slidesPerView:4,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
+//# sourceMappingURL=index.3918df2b.js.map
